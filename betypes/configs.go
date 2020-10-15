e50a46dd9ef6abe9f1746f8175c9efdfe866a865
Zong-Z <nazarslota20036@gmail.com>
2020-10-15 19:18:06 +0300

    Fix

--- betypes/configs.go
+++ betypes/configs.go
@@ -17,7 +17,7 @@ type Config struct {
 		DB       int    `json:"db"`
 	} `json:"redis_config"`
 	ChatsConfig struct {
-		QueueSize int `json:"queue_size"`
+		QueueSize uint `json:"queue_size"`
 	} `json:"chats_config"`
 }
 
